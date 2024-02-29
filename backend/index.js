f119861f10ae730077e8b51942e5d7fd8d89b842
Piyachon Rusuwannakul <piyachonrusuwannakul@Piyachons-MacBook-Pro.local>
2024-02-29 07:14:50 +0700

    index commit

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = require('./routes/router')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+const app =  express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 const corsOptions = {
     origin: '*',
-    credential: true,
+    credentials: true,
     optionSuccessStatus: 200
-
 }
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
+app.use('/', router)
 
-const port = 4000;
+const dbOptions = {useNewUrlParser:true, useUnifiedTopology:true}
+mongoose.connect(process.env.DB_URI, dbOptions)
+.then(() => console.log('DB Connected!'))
+.catch(err => console.log(err))
 
+const port = process.env.PORT || 4000
 const server = app.listen(port, () => {
-    console.log('server running at port ${port}');
+    console.log(`Server is running on port ${port}`)
 })
